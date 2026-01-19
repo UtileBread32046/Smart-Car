39d4c5a03723db9d74253676d2ae188840f2bf36
sssTNT <2676207227@qq.com>
2026-01-19 11:09:46 +0800

    修正马达接口反转问题, 添加Debug注释

--- include/move.h
+++ include/move.h
@@ -2,6 +2,16 @@
 #define MOVE_H
 #include <Arduino.h>
 
+/*  Debug须知!!!!!
+* 本项目基于 方舟教育科技全国青少年机器人技术等级考试五六级实操套装 编写
+* 询问淘宝客服后, 得知: 5q275aaI方舟教育科技的电子工程师没注意把PCB丝印搞反了
+* 实际控制逻辑为: MA和MB反了, MC和MD反了; 例如: 原MA引脚13,14实际连接在MB上, 原MB引脚15,16实际连接在MA上
+* 客服说: MA~MD每个端口的第一脚是控制方向的, 用数字输出; 第二脚是控制速度的, 用ledc
+* 本项目方案使用analogWrite, 新版本ESP32库(2.0+)可在底层自动调用ledc
+* 5bqf54mp54K55b+D5bCx6L+Z5rC05bmz6L+Y5LuW5aaI55qE5b2T55S15a2Q5bel56iL5biI5ZGiPz8/IOS9oOS7luWmiOWHuuWOgueahOaXtuWAmei/nua1i+ivlemDveS7luWmiOS4jeWBmuWQl+WwseeUn+S6p+WHuuS6hui/meS5iOWkmuW6n+WTgei/mOS7luWmiOebtOaOpeWOu+WNlj8g5L2g5Liq54uX55Wc55Sf5aW95q255b6A55uS5a2Q6YeM6KOF5Liq6K+05piO5Lmm5ZWKPw==
+* 据悉, 最早一批学习套件有此问题, 新版已修正过来了
+*/
+
 // 马达引脚
 const int motorLeftDir = 13;
 const int motorLeftSpd = 14;
